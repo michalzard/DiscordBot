72de2032de7dd69ff4cd6c5981f633933fbcda61
MichalPlatko <miso.platko@gmail.com>
2021-03-08 02:20:19 +0100

    init

---
+++ bot.js
@@ -0,0 +1,45 @@
+const Discord=require("discord.js");
+const client=new Discord.Client();
+require("dotenv").config();
+const fs=require("fs");
+const commandFiles=fs.readdirSync("./commands")
+
+client.commands=new Discord.Collection(); //holds array with all commandsd
+
+//Loads in all commands into collection
+for(let i=0;i<commandFiles.length;i++){
+    const cmd=require(`./commands/${commandFiles[i]}`);
+    client.commands.set(cmd.name,cmd);
+}
+
+client.on("ready",()=>{
+    console.log(`Logged in as ${client.user.tag}`)
+})
+
+client.login(process.env.BOT_TOKEN);
+
+
+client.on("message",message=>{
+if(message.author.bot) return;
+if(message.content.startsWith(process.env.BOT_PREFIX)){
+const [command,...args]=message.content
+.trim().substring(process.env.BOT_PREFIX.length).split(/\s+/);
+
+commandHandler(command,message,args);
+
+}
+})
+
+
+
+function commandHandler(command,message,args){
+    if(client.commands.get(command)==null) return;
+    if(client.commands.get(command).adminOnly){
+    if(message.member.hasPermission("ADMINISTRATOR"))
+    client.commands.get(command).execute(message,args);
+    else message.reply("Insufficient permissions");
+    } 
+    else client.commands.get(command).execute(message,args);
+}
+
+//message.member.hasPermission -> check perms of people
