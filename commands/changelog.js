0be5885d052aeab7f7f5c3bfebad0e1edb34056b
MichalPlatko <miso.platko@gmail.com>
2021-03-10 22:03:36 +0100

    added export so it doesnt break readdirasync

--- commands/changelog.js
+++ commands/changelog.js
@@ -4,3 +4,16 @@
  * FETCH GITHUB README.MD FILE AND DISPLAY CHANGENOTES,
  * NEED TO BE ABLE TO PARSE MARKDOWN USED BY GITHUB IN README FILES
  */
+module.exports = {
+    name: "changelog",
+    desc: "TODO changelog that will fetch data from readme.md from git",
+    adminOnly: false,
+
+    execute(message, args) {
+        try {
+          
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
